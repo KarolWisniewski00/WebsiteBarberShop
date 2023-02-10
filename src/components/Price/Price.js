d804a22d8a6d1f97bb2cb811d8e4f85c953c9db5
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2023-02-10 08:58:23 +0100

    new price

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -7,7 +7,7 @@ class Price extends React.Component {
                 <div className="px-6 py-12 md:px-12 text-gray-800 text-center lg:text-left">
                     <div className="container mx-auto xl:px-32">
                         <div className="grid lg:grid-cols-4 gap-12 flex items-center">
-                            <div className="mt-12 lg:mt-0 col-span-2 col-start-1 bg-black/50 p-10">
+                            <div className="mt-12 lg:mt-0 col-span-2 bg-black/50 p-5 md:p-10">
                                 <table className="table-auto w-full">
                                     <thead className="color-custom text-3xl">
                                         <tr>
@@ -16,89 +16,118 @@ class Price extends React.Component {
                                         </tr>
                                     </thead>
                                     <tbody className="text-stone-100">
-                                        <tr>
+                                        <tr className="border-b  md:border-none">
                                             <td>Strzyżenie damskie</td>
-                                            <td className="text-right py-3">80 - 150 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">80 - 150 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Modelowanie włosów</td>
+                                            <td className="text-right py-3 whitespace-nowrap">50 PLN - 100 PLN</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Koloryzacja jednolita</td>
-                                            <td className="text-right py-3">150 - 200 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">150 - 200 PLN</td>
                                         </tr>
-                                        <tr>
-                                            <td>Rozjaśnianie metodami (Air touch, Refleksy, Babylights, Dekoloryzacja)</td>
-                                            <td className="text-right py-3">350 - 500 PLN</td>
+                                        <tr className="border-b md:border-none">
+                                            <td>Rozjaśnianie metodami ( air touch, refleksy, babylights, dekoloryzacja)</td>
+                                            <td className="text-right py-3 whitespace-nowrap">350 - 500 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Keratynowe prostowanie</td>
-                                            <td className="text-right py-3">350 - 500 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">350 - 500 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Fryzury okolicznościowe</td>
-                                            <td className="text-right py-3">100 - 150 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">100 - 150 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Upięcia ślubne</td>
-                                            <td className="text-right py-3">200 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">200 PLN</td>
                                         </tr>
-                                        <tr>
-                                            <td>Pielęgnacja FibreClinix / ScalpClinix</td>
-                                            <td className="text-right py-3">80 - 100 PLN</td>
+                                        <tr className="border-b md:border-none">
+                                            <td>Pielęgnacja FibreClinix</td>
+                                            <td className="text-right py-3 whitespace-nowrap">80 - 100 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Pielęgnacja ScalpClinix (Kojący)</td>
+                                            <td className="text-right py-3 whitespace-nowrap">60 PLN +</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Pielęgnacja ScalpClinix (Kontrolujący Sebum)</td>
+                                            <td className="text-right py-3 whitespace-nowrap">60 PLN +</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Pielęgnacja ScalpClinix (Przeciwłupieżowy)</td>
+                                            <td className="text-right py-3 whitespace-nowrap">60 PLN +</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Pielęgnacja L'Oreal MetalDetox</td>
-                                            <td className="text-right py-3">50 - 80 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">50 - 80 PLN</td>
                                         </tr>
-                                        <tr>
-                                            <td>Pielęgnacja L'Oreal  (Regeneracja, Wygładzenie, Nawilżenie, Objętość)</td>
-                                            <td className="text-right py-3">55 PLN</td>
+                                        <tr className="border-b md:border-none">
+                                            <td>Pielęgnacja L'Oreal  ( regeneracja, wygładzenie, nawilżenie, objętość )</td>
+                                            <td className="text-right py-3 whitespace-nowrap">55 PLN</td>
                                         </tr>
-                                        <tr>
-                                            <td>Henna brwi</td>
-                                            <td className="text-right py-3">30 PLN</td>
+                                        <tr className="border-b md:border-none">
+                                            <td>Henna + regulacja brwi</td>
+                                            <td className="text-right py-3 whitespace-nowrap">30 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Henna rzęs</td>
-                                            <td className="text-right py-3">20 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">20 PLN</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Wąsik</td>
+                                            <td className="text-right py-3 whitespace-nowrap">20 PLN +</td>
                                         </tr>
                                         <tr>
                                             <td>Przedłużanie lub zagęszczanie włosów (różnymi metodami)</td>
-                                            <td className="text-right py-3 color-custom">Indywidualnie</td>
+                                            <td className="text-right py-3 color-custom font-bold">Indywidualnie</td>
                                         </tr>
                                     </tbody>
                                 </table>
-                                <table className="table-auto w-full">
+                                <table className="table-auto w-full mt-5">
                                     <thead className="color-custom text-3xl">
                                         <tr>
                                             <th>Barber</th>
                                         </tr>
                                     </thead>
                                     <tbody className="text-stone-100">
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Strzyżenie męskie</td>
-                                            <td className="text-right py-3">50 - 80 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">50 - 80 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Strzyżenie brody</td>
+                                            <td className="text-right py-3 whitespace-nowrap">70 PLN</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Strzyżenie męskie + broda (COMBO)</td>
-                                            <td className="text-right py-3">100 - 120 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">100 - 120 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Strzyżenie męskie + cover strzyżenia męskiego</td>
-                                            <td className="text-right py-3">120 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">120 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
+                                            <td>Strzyżenie brody + cover brody</td>
+                                            <td className="text-right py-3 whitespace-nowrap">140 PLN</td>
+                                        </tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Cover włosów</td>
-                                            <td className="text-right py-3">70 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">70 PLN</td>
                                         </tr>
-                                        <tr>
+                                        <tr className="border-b md:border-none">
                                             <td>Cover brody</td>
-                                            <td className="text-right py-3">70 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">70 PLN</td>
                                         </tr>
                                         <tr>
                                             <td>Golenie pełne głowy brzytwą</td>
-                                            <td className="text-right py-3">55 PLN</td>
+                                            <td className="text-right py-3 whitespace-nowrap">55 PLN</td>
                                         </tr>
                                     </tbody>
                                 </table>
+                                <div className="text-white py-3 color-custom font-bold mt-5">Ceny zależne są od długości oraz gęstości włosów. W przypadku ekstremalnie długich i/lub gęstych włosów cena jest ustalana indywidualnie</div>
                             </div>
                         </div>
                     </div>
